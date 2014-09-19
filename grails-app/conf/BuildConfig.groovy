75c0dc447b3a5d9a084baf3015585d477634b2e5
lyhcode <lyhcode@gmail.com>
2014-09-19 16:33:45 +0800

    add springsecurity core plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -72,5 +72,8 @@ grails.project.dependency.resolution = {
         //compile ":less-asset-pipeline:1.10.0"
         //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
+
+        // Spring Security Core Plugin
+        compile ":spring-security-core:2.0-RC4"
     }
 }
