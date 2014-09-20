9b3e6768e906c2a418b8021f4a7da31a6f93dfc4
lyhcode <lyhcode@gmail.com>
2014-09-20 19:18:29 +0800

    update og tags

--- slides.sh
+++ slides.sh
@@ -3,6 +3,6 @@ pandoc -t revealjs -V theme="$theme" -V revealjs-url="https://cdnjs.cloudflare.c
 sed -i .bak "s/simple/$theme/g" slides.html
 sed -i .bak "s/<head>/<head><meta property=\"og:image\" content=\"https:\/\/raw.githubusercontent.com\/lyhcode\/grails-realestate\/master\/slides.png\">/g" slides.html
 sed -i .bak "s/<head>/<head><meta property=\"og:type\" content=\"article\">/g" slides.html
-sed -i .bak "s/<head>/<head><meta property=\"og:title\" content=\"Groovy on Grails, TWJUG Sep 20, 2014\">/g" slides.html
-sed -i .bak "s/<head>/<head><meta property=\"og:description\" content=\"Introduction to Grails 2.4\">/g" slides.html
+sed -i .bak "s/<head>/<head><meta property=\"og:title\" content=\"Groovy on Grails\">/g" slides.html
+sed -i .bak "s/<head>/<head><meta property=\"og:description\" content=\"TWJUG Sep 20, 2014\">/g" slides.html
 
